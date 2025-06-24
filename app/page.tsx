78088d262da6359390ae36bbb2eefd26441ef8ad
TeejMcSteez <tjhall047@gmail.com>
2025-06-24 18:28:59 -0400

    Update root with new Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import Gallery from './components/Gallery'
 export default function Home() {
 	return (
 		<div className="flex flex-col min-w-screen min-h-screen bg-stone-300 bg-no-repeat bg-cover bg-origin-border text-black">
-			<Navbar/>	
+			<Navbar/>
+			<Hero />
 			<Gallery/>
 			{/* Currently client does not want about me section */}
 			<Contact/>
